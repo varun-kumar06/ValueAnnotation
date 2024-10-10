4fd652c536a01951e905196d56b0503ffcb17878
varun-kumar06 <kumarmvarun39@gmail.com>
2024-10-10 15:32:43 +0530

    First Commit

---
+++ src/main/java/com/sample/SpringAnnatationApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnatationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnnatationApplication.class, args);
+	}
+
+}
